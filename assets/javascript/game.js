4fb5a61c3eafa6507574e91d1e0e67a477c87755
Napoleon <wanderofrazgriz@gmail.com>
2018-06-23 17:13:54 -0700

    update js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,90 @@
 //array of words for the computer to choose from (object/array)
 
-var guessWords = [ "Nintendo", "Pong", "Atari", "Xbox", "Switch", "PlayStation", "Sega", "Sonic", "Mario", "Kratos", "Asteroids", "Forza", "Yoshi", "Sweet Tooth", "Master Chief", "PC", "Warcraft", "Starcraft", "Halo", "Call Of Duty", "Battlefield", "Gears Of War", "Splinter Cell", "Super Smash Brothers", "Fire Emblem", "Pokemon", "Infamous", "Fusion Frenzy", "Shadow Of The Colossus", "Persona", "Fable", "Between Good and Evil", "Half Life", "BioShock", "Red Dead Redemption", "DOTA", "Street Fighter", "Ryu", "Chun Li", "Devil May Cry", "Max Payne", "Grand Theft Auto","Heavy Rain", "Killzone", "Helghast","Ace Combat", "Spyro", "Crash Bandicoot", "Chrono Trigger", "Dragon Quest", "Mass Effect", "God Of War", "Sly Cooper", "Super Mario Sunshine", "Legend Of Zelda", "Turock"];
+    var guessWords = [ "Nintendo", "Pong", "Atari", "Xbox", "Switch", "PlayStation", "Sega", "Sonic", "Mario", "Kratos", "Asteroids", "Forza", "Yoshi", "Sweet Tooth", "Master Chief", "PC", "Warcraft", "Starcraft", "Halo", "Call Of Duty", "Battlefield", "Gears Of War", "Splinter Cell", "Super Smash Brothers", "Fire Emblem", "Pokemon", "Infamous", "Fusion Frenzy", "Shadow Of The Colossus", "Persona", "Fable", "Between Good and Evil", "Half Life", "BioShock", "Red Dead Redemption", "DOTA", "Street Fighter", "Ryu", "Chun Li", "Devil May Cry", "Max Payne", "Grand Theft Auto","Heavy Rain", "Killzone", "Helghast","Ace Combat", "Spyro", "Crash Bandicoot", "Chrono Trigger", "Dragon Quest", "Mass Effect", "God Of War", "Sly Cooper", "Super Mario Sunshine", "Legend Of Zelda", "Turock"];
 
 
-//create an array for the letters the user has guessed
+    //array for the letters the user has guessed
 
-var userGuess = [];
+    var userGuess = [];
 
-//create a for loop for the plac
+    //array for placeholder "_"
 
+    var placeholder = [];
 
-// Variables to track wins guesses remaining
+    //array for properly guessed words
 
-var wins = 0;
-var guessRemain = 8; //give users 8 guesses to guess the word
+    var correctGuess = [];
 
+    //current word index
 
-//create an array for the "_" placerholder
+    var currentWordIndex;
 
-var placeholder = [];
+    // Variables to track wins, guesses remaining
 
+    //const triesLeft = 10;
+    var wins = 0;
+    var guessRemain = 10; //give users 10 guesses to guess the word
 
-function startGame() {
+    //boolean response for when the game stops
+
+    var gameOver = true;
+
+    // computer randomly selects word from array
 
     var wordSelection = guessWords[Math.floor(Math.random() * guessWords.length)];
 
     console.log(wordSelection);
 
-    //create a for loop for the placeholder array so it'll mimic the word length for each word randomly selected
 
-    for (var i = 0; i < wordSelection.length; i++) {
-        placeholder[i] = "_";
-    } // for loop
+    function startGame() {
 
-    console.log(placeholder);
+        //loop for the placeholder array so it'll mimic the word length for each word randomly selected
 
+        for (var i = 0; i < wordSelection.length; i++) {
 
-} // function
-//startGame();
+            placeholder[i] = "_";
 
-document.onkeypress = function(event) {
+            document.getElementById("currentWordDiv").textContent = "Current Word: " + placeholder;
+        } // for loop
 
-    var letterPressed = event.key;
-
-   console.log(letterPressed);
-
-   //include if/else statements
+        console.log(placeholder);
 
 
+    } // function
+    //startGame();
 
-}; // fuction
+    document.onkeydown = function(event) {
 
-//through key functions, record what the user is pressing
+        var letterPressed = event.key;
 
-//"log" inputs into "letters guessed" div
+         console.log(letterPressed);
 
-//for letters that are correct, ensure letter appears in "current word" div, replacing the "_"
+        //include if/else statements
 
-//FOR each guess wrong, ensure the "guesses remaining" ticks down by one
+        if (letterPressed.indexOf(wordSelection) != -1) {
+            console.log();
+        }
 
-//VICTORY CONDITIONS EXECUTE: if all letters guessed correctly, execute 6.W: if not, execute 6.L. For BOTH Conditions, make sure the game refreshed for another attempt
+        if (wordSelection.includes(letterPressed)) { 
+        }
 
-// WIN (Optional) play win sound queues/songs
+        //for loop going through the word and comparing the words to the chosen, buiilding the placeholder array
 
-// LOSE (Optional) play loosing sound queues/songs
+        //look up join function in javascript
+    }
+
+    // fuction
+
+    //through key functions, record what the user is pressing
+
+    //"log" inputs into "letters guessed" div
+
+    //for letters that are correct, ensure letter appears in "current word" div, replacing the "_"
+
+    //FOR each guess wrong, ensure the "guesses remaining" ticks down by one
+
+    //VICTORY CONDITIONS EXECUTE: if all letters guessed correctly, execute 6.W: if not, execute 6.L. For BOTH Conditions, make sure the game refreshed for another attempt
+
+    // WIN (Optional) play win sound queues/songs
+
+    // LOSE (Optional) play loosing sound queues/songs
